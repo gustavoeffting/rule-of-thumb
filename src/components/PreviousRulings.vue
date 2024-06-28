6b9b80e66fc443c348aac941b9b08e13a347664c
Gustavo Effting <efftinggustavo94@gmail.com>
2024-06-27 21:18:57 -0300

    implement grid exibition type

--- src/components/PreviousRulings.vue
+++ src/components/PreviousRulings.vue
@@ -1,16 +1,20 @@
 <template>
   <div class="previous-rulings-header">
     <h2>Previous Rulings</h2>
-    <span>List (selector)</span>
+    <select class="select" v-model="exibitionType">
+      <option value="list" selected>List</option>
+      <option value="grid">Grid</option>
+    </select>
   </div>
-  <div v-for="ruling in previousRulings">
-    <RulingDetails :ruling="ruling" :picture="rulingPicture(ruling.picture)"/>
+  <div :class="{ 'ruling-details-grid': exibitionType === 'grid' }">
+    <RulingDetails v-for="ruling in previousRulings" :ruling="ruling" :picture="rulingPicture(ruling.picture)"
+      :exibitionType="exibitionType" />
   </div>
 </template>
 <script setup>
-import { computed } from 'vue';
-import RulingDetails from './RulingDetails.vue';
+import { computed, ref } from 'vue';
 import { useDataStore } from '../stores/dataStore';
+import RulingDetails from './RulingDetails.vue';
 import imgKanye from '../assets/img/kanye.png';
 import imgCristina from '../assets/img/cristina.png';
 import imgElon from '../assets/img/elon.png';
@@ -19,6 +23,8 @@ import imgMark from '../assets/img/mark.png';
 import imgGreta from '../assets/img/greta.png';
 
 const dataStore = useDataStore();
+
+const exibitionType = ref('list');
 
 const rulingPicture = (pictureName) => {
   switch (pictureName) {
@@ -43,6 +49,18 @@ const previousRulings = computed(() => dataStore.data);
 .previous-rulings-header {
   display: flex;
   justify-content: space-between;
+  align-items: center;
 }
 
+.select {
+  padding: 5px 10px;
+  width: 173px;
+  height: 36px;
+}
+
+.ruling-details-grid {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+}
 </style>
--- src/components/RulingDetails.vue
+++ src/components/RulingDetails.vue
@@ -1,8 +1,9 @@
 <template>
-  <div class="ruling-details" :style="{ backgroundImage: `linear-gradient(to right, rgba(0, 0, 0, 0.1) 60%, gray 100%), url(${picture})` }">
+  <div v-if="exibitionType === 'list'" class="ruling-details"
+    :style="{ backgroundImage: `linear-gradient(to right, rgba(0, 0, 0, 0.1) 60%, gray 100%), url(${picture})` }">
     <div class="ruling-info">
-      <button v-if="decision==='positive'" class="icon-button vote-result-info" aria-label="thumbs up">
-        <ThumbsUp alt="thumbs up"/>
+      <button v-if="decision === 'positive'" class="icon-button vote-result-info" aria-label="thumbs up">
+        <ThumbsUp alt="thumbs up" />
       </button>
       <button v-else class="icon-button vote-result-info" aria-label="thumbs down">
         <ThumbsDown alt="thumbs down" />
@@ -12,17 +13,21 @@
         <p class="ruling-description">{{ props.ruling.description }}</p>
       </div>
       <div class="ruling-info-buttons">
-        <p class="ruling-date">{{ hasVoted ? 'Thank you for your vote!' : `${lastUpdated} ago in ${props.ruling.category}` }}</p>
+        <p class="ruling-date">{{ hasVoted ? 'Thank you for your vote!' : `${lastUpdated} ago in
+          ${props.ruling.category}` }}</p>
         <div class="button-options">
           <div v-if="!hasVoted">
-            <button class="icon-button btn-option" :class="{ 'btn-vote-selected': vote === true }" @click="vote = true" aria-label="thumbs up">
-              <ThumbsUp alt="thumbs up"/>
+            <button class="icon-button btn-option" :class="{ 'btn-vote-selected': vote === true }" @click="vote = true"
+              aria-label="thumbs up">
+              <ThumbsUp alt="thumbs up" />
             </button>
-            <button class="icon-button btn-option" :class="{ 'btn-vote-selected': vote === false }" @click="vote = false" aria-label="thumbs down">
+            <button class="icon-button btn-option" :class="{ 'btn-vote-selected': vote === false }"
+              @click="vote = false" aria-label="thumbs down">
               <ThumbsDown alt="thumbs down" />
             </button>
           </div>
-          <button class="btn-vote" :class="{ 'btn-disabled': vote === null }" :disabled="vote === null" @click="handleVote()">
+          <button class="btn-vote" :class="{ 'btn-disabled': vote === null }" :disabled="vote === null"
+            @click="handleVote()">
             {{ hasVoted ? 'Vote Again' : 'Vote now' }}
           </button>
         </div>
@@ -30,7 +35,7 @@
     </div>
     <div class="vote-percentage">
       <div class="vote-percentage-positive" :style="{ width: averagePositive + '%' }">
-        <ThumbsUp alt="thumbs up"/>
+        <ThumbsUp alt="thumbs up" />
         <span>{{ averagePositive }}%</span>
       </div>
       <div class="vote-percentage-negative" :style="{ width: averageNegative + '%' }">
@@ -39,9 +44,52 @@
       </div>
     </div>
   </div>
+  <div v-else class="ruling-details-grid" :style="{ backgroundImage: `url(${picture})` }">
+    <div class="ruling-info-grid">
+      <button v-if="decision === 'positive'" class="icon-button vote-result-info-grid" aria-label="thumbs up">
+        <ThumbsUp alt="thumbs up" />
+      </button>
+      <button v-else class="icon-button vote-result-info-grid" aria-label="thumbs down">
+        <ThumbsDown alt="thumbs down" />
+      </button>
+      <p class="ruling-name-grid">{{ props.ruling.name }}</p>
+    </div>
+    <p class="ruling-description-grid">{{ props.ruling.description }}</p>
+    <div class="ruling-date-grid" style="display: flex; width: 100%; justify-content: end;">
+      <p class="ruling-date-grid">{{ hasVoted ? 'Thank you for your vote!' : `${lastUpdated} ago in
+        ${props.ruling.category}` }}</p>
+    </div>
+
+    <div class="button-options-grid">
+      <div v-if="!hasVoted">
+        <button class="icon-button btn-option-grid" :class="{ 'btn-vote-selected': vote === true }" @click="vote = true"
+          aria-label="thumbs up">
+          <ThumbsUp alt="thumbs up" />
+        </button>
+        <button class="icon-button btn-option-grid" :class="{ 'btn-vote-selected': vote === false }"
+          @click="vote = false" aria-label="thumbs down">
+          <ThumbsDown alt="thumbs down" />
+        </button>
+      </div>
+      <button class="btn-vote-grid" :class="{ 'btn-disabled': vote === null }" :disabled="vote === null"
+        @click="handleVote()">
+        {{ hasVoted ? 'Vote Again' : 'Vote now' }}
+      </button>
+    </div>
+    <div class="vote-percentage-grid">
+      <div class="vote-percentage-positive-grid" :style="{ width: averagePositive + '%' }">
+        <ThumbsUp alt="thumbs up" />
+        <span>{{ averagePositive }}%</span>
+      </div>
+      <div class="vote-percentage-negative-grid" :style="{ width: averageNegative + '%' }">
+        <span>{{ averageNegative }}%</span>
+        <ThumbsDown alt="thumbs down" />
+      </div>
+    </div>
+  </div>
 </template>
 <script setup>
-const props = defineProps(['ruling', 'picture']);
+const props = defineProps(['ruling', 'picture', 'exibitionType']);
 import ThumbsUp from '../assets/img/thumbs-up.svg?component';
 import ThumbsDown from '../assets/img/thumbs-down.svg?component';
 
@@ -177,5 +225,112 @@ const lastUpdated = computed(() => DateTime.fromISO(props.ruling.lastUpdated).se
 
 .btn-vote-selected {
   border: 1px solid white;
+}
+
+.ruling-details-grid {
+  display: block;
+  margin-top: 10px;
+  width: 348px;
+  height: 348px;
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+  color: white;
+}
+
+.ruling-info-grid {
+  display: flex;
+  justify-content: left;
+  align-items: center;
+  padding-top: 120px;
+}
+
+.ruling-name-grid {
+  white-space: nowrap;
+  overflow: hidden;
+  font-size: 28px;
+  text-align: left;
+  margin-left: 10px;
+}
+
+.vote-result-info-grid {
+  width: 30px;
+  height: 30px;
+}
+
+.ruling-description-grid {
+  max-width: 278px;
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  text-align: center;
+  padding: 0 34px;
+  margin: 0;
+  font-size: 14px;
+}
+
+.ruling-date-grid {
+  display: flex;
+  width: 100%;
+  justify-content: end;
+}
+
+.ruling-date-grid p {
+  margin-right: 30px;
+  font-size: 14px;
+  text-align: right;
+}
+
+.button-options-grid {
+  width: 100%;
+  display: flex;
+  justify-content: end;
+  align-items: center;
+  margin-right: 30px;
+}
+
+.btn-option-grid {
+  width: 30px;
+  height: 30px;
+  margin-left: 5px;
+  cursor: pointer;
+}
+
+.btn-vote-grid {
+  width: 107px;
+  height: 35px;
+  background-color: rgb(0, 0, 0, .5);
+  color: white;
+  border: 1px solid white;
+  margin-left: 5px;
+  cursor: pointer;
+}
+
+.vote-percentage-grid {
+  margin-top: 5px;
+  width: 100%;
+  height: 36px;
+  display: flex;
+  font-size: 20px;
+}
+
+.vote-percentage-positive-grid {
+  display: flex;
+  justify-content: start;
+  align-items: center;
+  gap: 5px;
+  background-color: rgb(60, 187, 180, 0.6);
+  height: 36px;
+  padding-left: 15px;
+}
+
+.vote-percentage-negative-grid {
+  display: flex;
+  justify-content: end;
+  align-items: center;
+  gap: 5px;
+  background-color: rgb(249, 172, 29, 0.6);
+  height: 36px;
+  padding-right: 15px;
 }
 </style>
